1809968af0e401b4074f41d43c476372e61b525c
Jimena Silva <jimenasilva@iMac-de-Jimena.local>
2024-07-02 21:52:33 -0400

    add logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { Col } from 'antd';
 import './App.css';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
+import logo from './statics/logo.svg'
 
 function App() {
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+        <img src={logo} alt="Pokedux" />
+      </Col>
       <Col span={8} offset={8}>
       <Searcher />
       </Col>
