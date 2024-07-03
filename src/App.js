7cd7e55bc486358d6a44f380dcd11d87c68d1f87
Jimena Silva <jimenasilva@iMac-de-Jimena.local>
2024-07-03 02:53:27 -0400

    add integration redux with hooks api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Col } from 'antd';
-import { connect } from 'react-redux'
 import './App.css';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import logo from './statics/logo.svg'
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getPokemon } from './api';
-import { setPokemons as setPokemonsActions } from './actions'
+import { setPokemons } from './actions'
+import { useDispatch, useSelector } from 'react-redux';
 
-function App({pokemons, setPokemons}) {
+function App() {
+
+  const pokemons = useSelector(state => state.pokemons)
+  const dispatch = useDispatch()
+
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon()
-      setPokemons(pokemonsRes)
+      dispatch(setPokemons(pokemonsRes))
     }
 
     fetchPokemons()
@@ -32,12 +36,5 @@ function App({pokemons, setPokemons}) {
   );
 }
 
-const mapStateToProps = (state => ({
-  pokemons: state.pokemons,
-}))
 
-const mapDispatchToProps = (dispatch => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value))
-}))
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
