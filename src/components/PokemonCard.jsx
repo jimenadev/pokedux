dfe2b03a7ab010a2c5f71edd7883377b03a28d26
Jimena Silva <jimenasilva@iMac-de-Jimena.local>
2024-07-02 22:05:42 -0400

    integration with api

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,10 +2,10 @@ import { Card } from 'antd'
 import Meta from 'antd/lib/card/Meta'
 import { StarOutlined } from '@ant-design/icons'
 
-const PokemonCard = () => {
+const PokemonCard = ({name}) => {
     return <Card
-        title="Ditto"
-        cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png" alt="Ditto" />}
+        title={name}
+        cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png" alt={name} />}
         extra={<StarOutlined />}
     >
         <Meta description="fire, magic" />
