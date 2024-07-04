02a4a02c5bd699ba55871fe8b739e68aba6d4596
Jimena Silva <jimenasilva@iMac-de-Jimena.local>
2024-07-04 01:17:17 -0400

    add pokemon types dynamically

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,13 +2,16 @@ import { Card } from 'antd'
 import Meta from 'antd/lib/card/Meta'
 import { StarOutlined } from '@ant-design/icons'
 
-const PokemonCard = ({name, image}) => {
+const PokemonCard = ({name, image, types}) => {
+
+    const typesString = types.map(elem => elem.type.name).join(', ')
+
     return <Card
         title={name}
         cover={<img src={image} alt={name} />}
         extra={<StarOutlined />}
     >
-        <Meta description="fire, magic" />
+        <Meta description={typesString} />
     </Card>
 }
 
