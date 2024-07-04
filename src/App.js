696b18d603668d41b2dd430fab88895ca20a1f79
Jimena Silva <jimenasilva@iMac-de-Jimena.local>
2024-07-04 01:10:29 -0400

    add loader

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { Col } from 'antd';
+import { Col, Spin } from 'antd';
 import './App.css';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import logo from './statics/logo.svg'
 import { useEffect } from 'react';
 import { getPokemon } from './api';
-import { getPokemonsWithDetails } from './actions'
+import { getPokemonsWithDetails, setLoading } from './actions'
 import { useDispatch, useSelector } from 'react-redux';
+
 
 function App() {
 
   const pokemons = useSelector(state => state.pokemons)
+  const loading = useSelector(state => state.loading)
   const dispatch = useDispatch()
 
   useEffect(() => {
     const fetchPokemons = async () => {
-      const pokemonsRes = await getPokemon()
-     
-      dispatch(getPokemonsWithDetails(pokemonsRes))
-    }
+    dispatch(setLoading(true))
+    const pokemonsRes = await getPokemon()
+    dispatch(getPokemonsWithDetails(pokemonsRes))
+    dispatch(setLoading(false))
+  }
 
-
-
-    fetchPokemons()
+  fetchPokemons()
   }, [])
 
 
@@ -32,9 +33,14 @@ function App() {
         <img src={logo} alt="Pokedux" />
       </Col>
       <Col span={8} offset={8}>
-      <Searcher />
+        <Searcher />
       </Col>
+      {loading ? 
+      <Col offset={12} >
+        <Spin spinning size='large' />
+        </Col> : 
       <PokemonList pokemons={pokemons}/>
+      }
     </div>
   );
 }
